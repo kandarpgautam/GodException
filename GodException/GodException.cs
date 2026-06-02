51a65321730566eba3a8fab9788e04af650f98b3
Kandarp Gautam <kandarp.gautam@tothenew.com>
2026-06-02 10:21:37 +0530

    Added: helpLink

--- GodException/GodException.cs
+++ GodException/GodException.cs
@@ -3,5 +3,6 @@
     public class GodException : Exception
     {
         override public string Message => "GodException: An error occurred. Ask God.";
+        override public string? HelpLink { get => "https://god.earth"; }
     }
 }
